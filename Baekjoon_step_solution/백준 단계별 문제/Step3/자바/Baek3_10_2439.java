889b118886840b2fd519d379773a6c3c1b73b68e
wink68 <cosihh55@gmail.com>
2022-07-29 22:05:37 +0900

    Baekjoon_step3_java

---
+++ Baekjoon_step_solution/백준 단계별 문제/Step3/자바/Baek3_10_2439.java
@@ -0,0 +1,30 @@
+/* 백준 2439번(step3-10): 별찍기 */
+/* JAVA - BufferedReader */
+
+package step3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek3_10_2439 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i=1; i<=N; i++) {             // i는 줄의 수
+			for (int j=1; j <= N-i; j++) {     // j는 공백의 수
+				System.out.print(" ");
+			}
+			for (int k=1; k<=i; k++) {         // k는 별의 수
+				System.out.print("*");
+			}
+			System.out.println();
+			br.close();
+		}
+	
+	}
+	
+}
