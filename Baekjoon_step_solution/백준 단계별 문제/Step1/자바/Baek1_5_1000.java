ec26af65857bc01386b84e820b6feaf5372e7c3e
Win K <108077414+wink68@users.noreply.github.com>
2022-08-07 03:28:44 +0900

    Update Baek1_5_1000.java

--- Baekjoon_step_solution/백준 단계별 문제/Step1/자바/Baek1_5_1000.java
+++ Baekjoon_step_solution/백준 단계별 문제/Step1/자바/Baek1_5_1000.java
@@ -1,4 +1,4 @@
-/* 백준 1000번(step1-5): A+B */
+/* 백준 1000번(step1-3): A+B */
 /* JAVA - BufferedReader, StringTokenizer */
 
 package step1;
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Baek1_5_1000 {
+public class Baek1_3_1000 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
