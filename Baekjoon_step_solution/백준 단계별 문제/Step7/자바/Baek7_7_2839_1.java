269faafb3d054944698452f6b8a5296104cfe95a
Win K <108077414+wink68@users.noreply.github.com>
2022-08-07 01:48:43 +0900

    Update Baek7_7_2839_1.java

--- Baekjoon_step_solution/백준 단계별 문제/Step7/자바/Baek7_7_2839_1.java
+++ Baekjoon_step_solution/백준 단계별 문제/Step7/자바/Baek7_7_2839_1.java
@@ -17,11 +17,11 @@ public class Baek7_7_2839_1 {
 		}
 		
 		else if ((N>=3) && ((N % 5) == 1 || (N % 5) == 3)) {     // 5로 나누었을 때, 나머지가 1 or 3인 경우
-			System.out.println((N / 5) +1);                      // 5의 봉지 갯수 + 1
+			System.out.println((N / 5) +1);                  // 5의 봉지 갯수 + 1
 		}
 		
 		else if ((N>=8) && ((N % 5) == 4 || (N % 5) == 2)) {     // 5로 나누었을 때, 나머지가 4 or 2인 경우
-			System.out.println((N / 5) +2);                      // 5의 봉지 갯수 + 2
+			System.out.println((N / 5) +2);                  // 5의 봉지 갯수 + 2
 		}
 		
 		else if (N == 4 || N == 7) {
