a653fb784c8f52dd519f529f50f0b7158b185153
Win K <108077414+wink68@users.noreply.github.com>
2022-08-10 17:57:59 +0900

    Update Baek2440.java

--- Baekjoon_step_solution/백준 전체 문제/자바/반복문/Baek2440.java
+++ Baekjoon_step_solution/백준 전체 문제/자바/반복문/Baek2440.java
@@ -15,7 +15,7 @@ public class Baek2440 {
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		for (int i=1; i <= N; i++) {           // i는 줄의 수
+		for (int i=1; i <= N; i++) {               // i는 줄의 수
 			for (int j=1; j <= N-i+1; j++) {   // j는 별의 수, j가 N이 될때까지 별 찍기
 				System.out.print(STAR);
 			}
