60e7cbe42631238f4591d1745bf5518f0c4096e2
Win K <108077414+wink68@users.noreply.github.com>
2022-08-13 17:22:04 +0900

    Update Baek7_5_10250.java

--- Baekjoon_step_solution/백준 단계별 문제/Step7/자바/Baek7_5_10250.java
+++ Baekjoon_step_solution/백준 단계별 문제/Step7/자바/Baek7_5_10250.java
@@ -14,26 +14,25 @@ public class Baek7_5_10250 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
-		int T = Integer.parseInt(br.readLine());  // 테스트 개수
+		int T = Integer.parseInt(br.readLine());    // 테스트 개수
 		
 		for (int i=0; i < T; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			
-			int H = Integer.parseInt(st.nextToken());  // N번째 손님
-			st.nextToken();                            // 방의 수는 불필요
-			int N = Integer.parseInt(st.nextToken());  // H는 건물 층수
+			int H = Integer.parseInt(st.nextToken());   // N번째 손님
+			st.nextToken();                             // 방의 수는 불필요
+			int N = Integer.parseInt(st.nextToken());   // H는 건물 층수
 			
 			
 			// 배정: Y는 층수, X는 호수 (엘리베이터로부터 떨어진 거리)
-			int X, Y;
 			if (N % H == 0) {   // 꼭대기 층, 꼭대기층은 몫 = 호수
-				Y = H *100;
-				X = N / H;
+				int Y = H *100;
+				int X = N / H;
 				sb.append(Y + X).append('\n');
 				
 			} else {
-				Y = (N % H) *100;
-				X = (N / H) +1;  // 몫이 호수, 몫이 0일때 맨 처음 1호수가 있기에 +1
+				int Y = (N % H) *100;
+				int X = (N / H) +1;  // 몫이 호수, 몫이 0일때 맨 처음 1호수가 있기에 +1
 				sb.append(Y + X).append('\n');
 			}
 		}
