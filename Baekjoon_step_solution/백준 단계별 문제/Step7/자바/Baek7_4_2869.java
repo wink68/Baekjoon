67b095bbf1e36b59d3b71e13b3e2624b7988cb07
Win K <108077414+wink68@users.noreply.github.com>
2022-08-13 17:13:50 +0900

    Update Baek7_4_2869.java

--- Baekjoon_step_solution/백준 단계별 문제/Step7/자바/Baek7_4_2869.java
+++ Baekjoon_step_solution/백준 단계별 문제/Step7/자바/Baek7_4_2869.java
@@ -15,15 +15,18 @@ public class Baek7_4_2869 {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
 		// 정점에 도착하면 멈춘다
-		long A = Long.parseLong(st.nextToken());  // up
-		long B = Long.parseLong(st.nextToken());  // down
-		long V = Long.parseLong(st.nextToken());  // length
-		long rest = V - A;  // 남은 거리
-		long move = A - B;  // 하루에 갈 수 있는 거리
+		long A = Long.parseLong(st.nextToken());   // up
+		long B = Long.parseLong(st.nextToken());   // down
+		long V = Long.parseLong(st.nextToken());   // length
+		
+		long rest = V - A;   // 남은 거리
+		long move = A - B;   // 하루에 갈 수 있는 거리
+		
 		
 		// up = 3, down = 1, length = 7, 딱 맞게 정점에 도착할 경우 (3일 걸림)
 		if (rest % move == 0) {
 			System.out.println(rest / move +1);
+			
 		// up = 3, down = 1, length = 8, 정점을 초과해서 도착할 경우 (4일 걸림)
 		} else {
 			System.out.println(rest / move +2);
